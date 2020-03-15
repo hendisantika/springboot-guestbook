4cefb8bed35131754dcd37ec3e3723364d89b263
Hendi Santika <hendisantika@gmail.com>
2020-03-16 06:48:32 +0700

    #15 - Add testMapping API in GuestBookRestController

--- src/main/java/com/hendisantika/springbootguestbook/controller/GuestBookRestController.java
+++ src/main/java/com/hendisantika/springbootguestbook/controller/GuestBookRestController.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springbootguestbook.controller;
 
+import com.hendisantika.springbootguestbook.domain.GuestBook;
 import com.hendisantika.springbootguestbook.service.GuestBookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +24,9 @@ public class GuestBookRestController {
 
     @Autowired
     private GuestBookService guestBookService;
+
+    @GetMapping("/comments")
+    public List<GuestBook> testMapping() {
+        return guestBookService.findAllEntries();
+    }
 }
