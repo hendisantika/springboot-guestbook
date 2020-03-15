12eeac77053e5355cd30988e7deca0615f8ea4aa
Hendi Santika <hendisantika@gmail.com>
2020-03-16 06:50:27 +0700

    #21 - Add GuestBookWebController

---
+++ src/main/java/com/hendisantika/springbootguestbook/controller/GuestBookWebController.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springbootguestbook.controller;
+
+import com.hendisantika.springbootguestbook.service.GuestBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-guestbook
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/03/20
+ * Time: 06.50
+ */
+@Controller
+public class GuestBookWebController {
+
+    private static final String ENTRIES_TEMPLATE_ID = "entries";
+    private static final String NEW_ENTRY_TEMPLATE_ID = "newEntry";
+    private static final String GUESTBOOK_FORM_HEADER_ID = "formHeader";
+    private static final String GUESTBOOK_TEMPLATE = "guestbook";
+    private static final String HOMEPAGE_REDIRECT = "redirect:/";
+    @Autowired
+    private GuestBookService guestBookService;
+
+}
