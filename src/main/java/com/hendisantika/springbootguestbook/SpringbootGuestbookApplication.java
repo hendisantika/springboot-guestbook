ec13a274c724ebefe164be1a2c461d1d01f0d604
Hendi Santika <hendisantika@gmail.com>
2020-03-15 10:40:01 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootguestbook/SpringbootGuestbookApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootguestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGuestbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGuestbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootguestbook/SpringbootGuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootguestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGuestbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
