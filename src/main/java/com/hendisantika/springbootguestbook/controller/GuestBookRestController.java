c06c5c7616c86e2cc47e053648c6c4fa5620cc75
Hendi Santika <hendisantika@gmail.com>
2020-03-16 06:49:42 +0700

    #19 - Add addComment API in GuestBookRestController

--- src/main/java/com/hendisantika/springbootguestbook/controller/GuestBookRestController.java
+++ src/main/java/com/hendisantika/springbootguestbook/controller/GuestBookRestController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +47,10 @@ public class GuestBookRestController {
     @DeleteMapping("/comment/{id}")
     public void deleteGuestBookEntryById(@PathVariable("id") Integer id) {
         this.guestBookService.deleteGuestBookEntryById(id);
+    }
+
+    @PostMapping("/add")
+    public void addComment(@RequestBody GuestBook guestBookEntry) {
+        this.guestBookService.save(guestBookEntry);
     }
 }
