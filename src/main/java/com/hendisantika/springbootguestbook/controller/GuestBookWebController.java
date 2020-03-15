3c3e51321bd9c6f769e9b828e220ad045ea836ad
Hendi Santika <hendisantika@gmail.com>
2020-03-16 06:50:55 +0700

    #22 - Add displayGuestBook in GuestBookWebController

--- src/main/java/com/hendisantika/springbootguestbook/controller/GuestBookWebController.java
+++ src/main/java/com/hendisantika/springbootguestbook/controller/GuestBookWebController.java
@@ -1,8 +1,11 @@
 package com.hendisantika.springbootguestbook.controller;
 
+import com.hendisantika.springbootguestbook.domain.GuestBook;
 import com.hendisantika.springbootguestbook.service.GuestBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,7 +24,17 @@ public class GuestBookWebController {
     private static final String GUESTBOOK_FORM_HEADER_ID = "formHeader";
     private static final String GUESTBOOK_TEMPLATE = "guestbook";
     private static final String HOMEPAGE_REDIRECT = "redirect:/";
+
     @Autowired
     private GuestBookService guestBookService;
 
+    @GetMapping("/")
+    public String displayGuestBook(Model model) {
+
+        model.addAttribute(GUESTBOOK_FORM_HEADER_ID, "Add a New Comment");
+        model.addAttribute(ENTRIES_TEMPLATE_ID, this.guestBookService.findAllEntries());
+        model.addAttribute(NEW_ENTRY_TEMPLATE_ID, new GuestBook());
+
+        return GUESTBOOK_TEMPLATE;
+    }
 }
